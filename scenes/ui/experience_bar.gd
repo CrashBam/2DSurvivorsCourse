12750210a452872b30562baefdf1a6e9b13a2194
CrashBam <37989168+CrashBam@users.noreply.github.com>
2023-10-13 18:56:00 -0700

    pro bar

---
+++ scenes/ui/experience_bar.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+@export var experience_manager: Node
+@onready var progress_bar = $MarginContainer/ProgressBar
+
+func _ready():
+	progress_bar.value = 0
+	experience_manager.experience_updated.connect(on_experience_updated)
+	
+	
+func on_experience_updated(current_experience: float, target_experience):
+	var percent = current_experience / target_experience # todo check if not 0
+	progress_bar.value = percent
