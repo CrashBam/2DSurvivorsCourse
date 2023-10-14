11e9fa9a21293b23ede9e7ceb8a3779f2b22b8d0
CrashBam <37989168+CrashBam@users.noreply.github.com>
2023-10-14 16:54:23 -0700

    no

--- scenes/manager/upgrade_manager.gd
+++ scenes/manager/upgrade_manager.gd
@@ -17,7 +17,7 @@ func on_level_up(current_level: int):
 	
 	var upgrade_screen_instance = upgrade_screen_scene.instantiate()
 	add_child(upgrade_screen_instance)
-	upgrade_screen_instance.set_ability_upgrades([chosen_upgrade] as Array[AbilityUpgrade])
+	upgrade_screen_instance.set_ability_upgrade([chosen_upgrade] as Array[AbilityUpgrade])
 
 
 
--- scenes/ui/upgrade_screen.gd
+++ scenes/ui/upgrade_screen.gd
@@ -2,7 +2,7 @@ extends CanvasLayer
 
 @export var upgrade_card_scene: PackedScene
 
-@onready var card_container: HBoxContainer = $%CardContainer
+@onready var card_container: HBoxContainer = %CardContainer
 
 func set_ability_upgrade(upgrades: Array[AbilityUpgrade]):
 	for upgrade in upgrades:
