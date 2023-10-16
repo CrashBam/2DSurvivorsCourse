1ad12b96d9e05f5e81f3e6b6393116f50bb13e1a
CrashBam <37989168+CrashBam@users.noreply.github.com>
2023-10-15 19:06:51 -0700

    fixed the canvas bug all is working as of oct15

--- scenes/manager/experience_manager.gd
+++ scenes/manager/experience_manager.gd
@@ -7,7 +7,7 @@ const TARGET_EXPERIENCE_GROWTH = 5
 
 var current_experience = 0
 var current_level = 1
-var target_experience = 5
+var target_experience = 1 # set to 5, 1 = we are playing
 
 func _ready():
 	GameEvents.experience_vial_collected.connect(on_experience_vial_collected)
