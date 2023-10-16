16c1195d27131a7cf204ec54f1014c53b2ab3f2c
CrashBam <37989168+CrashBam@users.noreply.github.com>
2023-10-15 18:39:50 -0700

    so far working

--- scenes/ui/upgrade_screen.gd
+++ scenes/ui/upgrade_screen.gd
@@ -1,1 +1,15 @@
 extends CanvasLayer
+
+@export var upgrade_card_scene: PackedScene
+
+@onready var card_container: HBoxContainer = %CardContainer
+
+# pass in an array of ability upgrades to display on the update screen
+func set_ability_upgrades(upgrades: Array[AbilityUpgrade]):
+	
+	for upgrade in upgrades:
+		var card_instance = upgrade_card_scene.instantiate()
+		card_container.add_child(card_instance)
+		card_instance.set_ability_upgrade(upgrade)
+		
+		
