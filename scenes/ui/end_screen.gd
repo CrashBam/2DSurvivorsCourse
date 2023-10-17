c55891a22dc8b1021dd3510f55b4b104ba600450
CrashBam <37989168+CrashBam@users.noreply.github.com>
2023-10-16 18:46:28 -0700

    got win loose in the game

--- scenes/ui/end_screen.gd
+++ scenes/ui/end_screen.gd
@@ -5,14 +5,20 @@ func _ready():
 	get_tree().paused = true
 	%RestartButton.pressed.connect(on_restart_pressed)
 	%QuitButton.pressed.connect(on_quit_pressed)
-	
-	
+
+
+func set_defeat():
+	%TitleLabel.text = "Defeat"
+	%DescriptionLabel.text = "You lost!"
+
+
 func on_restart_pressed():
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
-	
-	
+
 
 func on_quit_pressed():
 	get_tree().quit()
 	
+
+
