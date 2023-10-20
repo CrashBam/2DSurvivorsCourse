eecd9d2e41c4c4ae5c70683c220719a25436ebdc
CrashBam <37989168+CrashBam@users.noreply.github.com>
2023-10-19 17:37:37 -0700

    arena_difficulty_increased function added
    arena_difficulty_increased added with timers under arena_manager and Arena timermanager

--- scenes/manager/arena_time_manager.gd
+++ scenes/manager/arena_time_manager.gd
@@ -1,13 +1,27 @@
 extends Node
 
+signal arena_difficulty_increased(arena_difficulty: int)
+
+const DIFFICULTY_INTERVAL = 5
+
 @export var end_screen_scene: PackedScene
 
 @onready var timer = $Timer
 
-
+var arean_difficulty = 0
 
 func _ready():
 	timer.timeout.connect(on_timer_timeout)
+
+
+func _process(delta):
+	# the target goes down 5 sec everytime arean_difficulty goes up by 1 
+	var next_time_target = timer.wait_time - ((arean_difficulty +1) * DIFFICULTY_INTERVAL)
+	if timer.time_left <= next_time_target:
+		arean_difficulty += 1
+		arena_difficulty_increased.emit(arean_difficulty)
+		
+
 
 func get_time_elapsed():
 	return timer.wait_time - timer.time_left
--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -3,14 +3,22 @@ extends Node
 const SPAWN_RADIUS = 375
 
 @export var basic_enemy_scene: PackedScene # after : it's type of PackedScene
+@export var arena_time_manager: Node
 
+@onready var timer = $Timer
+
+var base_spawn_time = 0
 
 func _ready():
+	base_spawn_time = timer.wait_time
 	# to get the timer you use $Timer. (then the call you want)
-	$Timer.timeout.connect(on_timer_timeout)
-	
+	timer.timeout.connect(on_timer_timeout)
+	arena_time_manager.arena_difficulty_increased.connect(on_arena_difficulty_increased)
 	
 func on_timer_timeout():
+	# timer.start() is change the wait time in func on_area_difficulty_increased():
+	# then it will update on the next timeout signal.
+	timer.start()
 	# Spawn the enemy outside the view of the player is what we are going for
 	var player = get_tree().get_first_node_in_group("player") as Node2D
 	if player == null: return
@@ -27,6 +35,14 @@ func on_timer_timeout():
 	var entities_layer = get_tree().get_first_node_in_group("entities_layer")
 	entities_layer.add_child(enemy)
 	enemy.global_position = spawn_position
+	
+# increase 
+func on_arena_difficulty_increased(arena_difficlty: int):
+	var time_off = (.1 /12) * arena_difficlty
+	# if time_off exceeded .7 - clamp it at .7 
+	time_off = min(time_off, .7) 
+	print(time_off)
+	timer.wait_time = base_spawn_time - time_off
 	
 
 
