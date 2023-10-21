f674b48796edb22ab15ab5e72944947c3af3d5ba
CrashBam <37989168+CrashBam@users.noreply.github.com>
2023-10-20 17:58:05 -0700

    weird

--- scenes/manager/upgrade_manager.gd
+++ scenes/manager/upgrade_manager.gd
@@ -11,22 +11,7 @@ var current_upgrades = {}
 func _ready():
 	experience_manager.level_up.connect(on_level_up)
 	
-	
-func on_level_up(current_level: int):
-	# We choose which upgrades we want to show
-	var chosen_upgrade = upgrade_pool.pick_random() as AbilityUpgrade
-	if chosen_upgrade == null :
-		return
-		
-	# we instantiate and add it as a child 
-	var upgrade_screen_instance = upgrade_screen_scene.instantiate()
-	add_child(upgrade_screen_instance)
-	# we tell it, these are the upgrades to show
-	upgrade_screen_instance.set_ability_upgrades([chosen_upgrade] as Array[AbilityUpgrade])
-	# listening for a click, if so call (on_upgrade_selected)
-	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
-	
-	
+
 func apply_upgrade(upgrade: AbilityUpgrade):
 	var has_upgrade = current_upgrades.has(upgrade.id)
 	if !has_upgrade:
@@ -39,6 +24,30 @@ func apply_upgrade(upgrade: AbilityUpgrade):
 	
 	GameEvents.emit_ability_upgrade_added(upgrade, current_upgrades) 
 
+func pick_upgrades():
+	var chosen_upgrades: Array[AbilityUpgrade] = []
+	var filtered_upgrades = upgrade_pool.duplicate() # duplicate the array
+	for i in 2:
+		# chosen_upgrade will return every upgrade in the list does not share id of the chosen upgrade
+		var chosen_upgrade = filtered_upgrades.pick_random() as AbilityUpgrade
+		# add it
+		chosen_upgrades.append(chosen_upgrade)
+		filtered_upgrades = filtered_upgrades.filter(func (upgrade): return upgrade.id != chosen_upgrade.id)
+	
+	return chosen_upgrades
+
 
 func on_upgrade_selected(upgrade: AbilityUpgrade):
 	apply_upgrade(upgrade)
+	
+	
+func on_level_up(current_level: int):
+	# we instantiate and add it as a child 
+	var upgrade_screen_instance = upgrade_screen_scene.instantiate()
+	add_child(upgrade_screen_instance)
+	var chosen_upgrades = pick_upgrades()
+	# we tell it, these are the upgrades to show
+	upgrade_screen_instance.set_ability_upgrades(chosen_upgrades as Array[AbilityUpgrade])
+	# listening for a click, if so call (on_upgrade_selected)
+	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
+
