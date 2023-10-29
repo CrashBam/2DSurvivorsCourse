98ffa189e3011c5f8bea37aa10be6d65c184574c
CrashBam <37989168+CrashBam@users.noreply.github.com>
2023-10-29 06:57:05 -0700

    text fixing and pop ups

--- scenes/game_object/game_camera/game_camera.gd
+++ scenes/game_object/game_camera/game_camera.gd
@@ -1,19 +1,21 @@
 extends Camera2D
 
+@onready var player_nodes = get_tree().get_nodes_in_group("player")
+
 var target_position = Vector2.ZERO
+
 
 func _ready():
 	make_current()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	acquire_target()
 	global_position = global_position.lerp(target_position, 1.0 - exp(-delta *20))
 
 
 func acquire_target():
-	var player_nodes = get_tree().get_nodes_in_group("player")
+	#var player_nodes = get_tree().get_nodes_in_group("player")
 	if player_nodes.size() > 0:
 		var player = player_nodes[0] as Node2D
 		
--- scenes/ui/end_screen.gd
+++ scenes/ui/end_screen.gd
@@ -1,7 +1,18 @@
 extends CanvasLayer
 
+@onready var panel_container = %PanelContainer
+
 
 func _ready():
+	# set pivot off set 1/2 height, 1/2 width = center
+	panel_container.pivot_offset = panel_container.size / 2
+	# create tween to be played
+	var tween = create_tween()
+	# we reset to Zero, godot work around
+	tween.tween_property(panel_container, "scale", Vector2.ZERO, 0)
+	tween.tween_property(panel_container, "scale", Vector2.ONE, .3)\
+	.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
+	
 	get_tree().paused = true
 	%RestartButton.pressed.connect(on_restart_pressed)
 	%QuitButton.pressed.connect(on_quit_pressed)
