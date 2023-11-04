1e9bf6c4b14234c8ef59a24526c3e671b5cccda2
CrashBam <37989168+CrashBam@users.noreply.github.com>
2023-11-03 19:01:57 -0700

    splash in

--- scenes/autoload/screen_transition.gd
+++ scenes/autoload/screen_transition.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
 signal transitioned_halfway
+signal transition_done
 
 @onready var animation_player = $AnimationPlayer
 
@@ -24,4 +25,15 @@ func emit_transitioned_halfway():
 		skip_emit = false
 		return
 	transitioned_halfway.emit()
+
+
+func transition_backwards():
+	$ColorRect.visible = true
+	animation_player.play_backwards("default")
+	await animation_player.animation_finished
+	transitioned_halfway.emit()
+	animation_player.play("default")
+	await animation_player.animation_finished
+	$ColorRect.visible = false
+
 
--- scenes/splash_screen/splash_screen.gd
+++ scenes/splash_screen/splash_screen.gd
@@ -1,26 +1,21 @@
 extends Node
 
-var main_menu_scene = preload("res://scenes/ui/main_menu.tscn")
 
 @onready var animation_player = $AnimationPlayer
 @onready var timer = $Timer
 
-@export var pause_time: float = 1
 
 func _ready():
-	# Start playing the "start_logo" animation
 	animation_player.play("start_logo")
 
-	# Connect the Timer's timeout signal to the load_main_menu function
-	#timer.connect("timeout", self, "load_main_menu")
 
-	# Set the Timer's wait time to the desired pause_time
-	#timer.wait_time = pause_time
-	#timer.start()
+func _on_animation_player_animation_finished(anim_name):
+	print(anim_name + " - add more stuff here")
+	timer.start()
 	
-# This function is called when it's time to load the main menu
-func load_main_menu():
-	print("Load main menu called")
-	#var main_menu_instance = main_menu_scene.instance()
-	#get_tree().get_root().add_child(main_menu_instance)
 
+
+func _on_timer_timeout():
+	ScreenTransition.transition()
+	await ScreenTransition.transitioned_halfway
+	get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
