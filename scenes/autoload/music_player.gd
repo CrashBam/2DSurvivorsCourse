2d071aef4717d007a40715beabfc780c76b3917a
CrashBam <37989168+CrashBam@users.noreply.github.com>
2023-11-04 07:50:23 -0700

    good

--- scenes/autoload/music_player.gd
+++ scenes/autoload/music_player.gd
@@ -1,11 +1,20 @@
 extends AudioStreamPlayer
+class_name MusicPlayerNodeClass
 
 @onready var timer = $Timer
+@onready var start_time_wait = $StartTimeWait
 
 
 func _ready():
 	finished.connect(on_finished)
 	timer.timeout.connect(on_timer_timeout)
+	
+	start_time_wait.start()
+	start_time_wait.timeout.connect(on_timer_timeout)
+
+
+func _onEndSplashScreen():
+	pass
 
 func on_finished():
 	timer.start()
@@ -13,3 +22,4 @@ func on_finished():
 
 func on_timer_timeout():
 	play()
+	start_time_wait.stop()
--- scenes/splash_screen/splash_screen.gd
+++ scenes/splash_screen/splash_screen.gd
@@ -1,13 +1,35 @@
 extends Node
+class_name SplashScreen
+
+signal end_splash_screen
 
 
 @onready var animation_player = $AnimationPlayer
 @onready var timer = $Timer
+@onready var audio_player = $AudioStreamPlayer2D
+@onready var audio_stream_player_2d_swoosh = $AudioStreamPlayer2DSwoosh
 
 
 func _ready():
 	animation_player.play("start_logo")
+	end_splash_screen.connect(on_end_splash_screen)
 
+func _input(event):
+	if(event is InputEventKey):
+		end_splash_screen.emit()
+		_on_timer_timeout()
+
+
+func on_end_splash_screen():
+	pass
+
+
+func play_sound_sfx():
+	audio_player.play()
+
+
+func play_swoosh_sfx():
+	audio_stream_player_2d_swoosh.play()
 
 func _on_animation_player_animation_finished(anim_name):
 	print(anim_name + " - add more stuff here")
@@ -18,4 +40,5 @@ func _on_animation_player_animation_finished(anim_name):
 func _on_timer_timeout():
 	ScreenTransition.transition()
 	await ScreenTransition.transitioned_halfway
+	end_splash_screen.emit()
 	get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
