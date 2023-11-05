815cc6129b91663242af0c87ce633c3c01d15f83
CrashBam <37989168+CrashBam@users.noreply.github.com>
2023-11-04 19:01:46 -0700

    good splash screen

--- scenes/autoload/music_player.gd
+++ scenes/autoload/music_player.gd
@@ -1,20 +1,17 @@
 extends AudioStreamPlayer
-class_name MusicPlayerNodeClass
 
 @onready var timer = $Timer
-@onready var start_time_wait = $StartTimeWait
-
 
 func _ready():
 	finished.connect(on_finished)
 	timer.timeout.connect(on_timer_timeout)
 	
-	start_time_wait.start()
-	start_time_wait.timeout.connect(on_timer_timeout)
+	MySignals.SplashScreenEnded.connect(on_splashscreen_ended)
 
 
-func _onEndSplashScreen():
-	pass
+func on_splashscreen_ended():
+	play()
+
 
 func on_finished():
 	timer.start()
@@ -22,4 +19,4 @@ func on_finished():
 
 func on_timer_timeout():
 	play()
-	start_time_wait.stop()
+
--- scenes/splash_screen/splash_screen.gd
+++ scenes/splash_screen/splash_screen.gd
@@ -1,27 +1,24 @@
 extends Node
 class_name SplashScreen
 
-signal end_splash_screen
-
 
 @onready var animation_player = $AnimationPlayer
 @onready var timer = $Timer
 @onready var audio_player = $AudioStreamPlayer2D
 @onready var audio_stream_player_2d_swoosh = $AudioStreamPlayer2DSwoosh
+@onready var logo_blinking_anim = $CenterContainer/VBoxContainer/LogoBlinkingAnim
+@onready var logo_blink_sprite = $CenterContainer/VBoxContainer/LogoBlinkSprite
 
 
 func _ready():
 	animation_player.play("start_logo")
-	end_splash_screen.connect(on_end_splash_screen)
+	logo_blinking_anim.hide()
+	logo_blink_sprite.show()
 
 func _input(event):
 	if(event is InputEventKey):
-		end_splash_screen.emit()
+		MySignals.SplashScreenEnded.emit()
 		_on_timer_timeout()
-
-
-func on_end_splash_screen():
-	pass
 
 
 func play_sound_sfx():
@@ -31,14 +28,18 @@ func play_sound_sfx():
 func play_swoosh_sfx():
 	audio_stream_player_2d_swoosh.play()
 
+
 func _on_animation_player_animation_finished(anim_name):
-	print(anim_name + " - add more stuff here")
 	timer.start()
+	logo_blink_sprite.hide()
+	logo_blinking_anim.show()
+	logo_blinking_anim.play("blinking")
+	
 	
 
 
 func _on_timer_timeout():
 	ScreenTransition.transition()
 	await ScreenTransition.transitioned_halfway
-	end_splash_screen.emit()
+	MySignals.SplashScreenEnded.emit()
 	get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
